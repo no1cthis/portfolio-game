35d2b295fb029efa441e21e3b9055c34b603b5b0
no1cthis <zippy3431@gmail.com>
2022-08-18 00:55:22 +0300

    fix link

--- src/components/Boards/HolographBoard/HolographBoard.js
+++ src/components/Boards/HolographBoard/HolographBoard.js
@@ -5,7 +5,7 @@ function HolographBoard() {
     return ( 
               <Modal title={'Holograph Board'}>
                     I practiced in framer motion. It was landing page, I cut it to practice changing page animation, so it might not be fullscreen
-                    <a href="https://no1cthis.github.io/holograph/">Github</a>
+                    <a href="https://github.com/no1cthis/holograph">Github</a>
               </Modal>
      );
 }
